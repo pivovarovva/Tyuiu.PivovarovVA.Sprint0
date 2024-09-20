d29e4d84597ecb207962a57e35c20e4191e71c5e
Admin <Admin@DESKTOP-34SQLP8>
2024-09-20 17:28:01 +0500

    Task1

--- Tyuiu.PivovarovVA.Sprint0/Program.cs
+++ Tyuiu.PivovarovVA.Sprint0/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Tyuiu.SpirinIS.Sprint0.Task1.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Вывод на консоль строку Hello, World!
+            Console.WriteLine("Hello, World!");
+
+            // Объявляем переменные
+            string name = "Владислав";
+            int age = 17;
+
+            // Вывод на консоль строку
+            Console.WriteLine("Здравствуйте, меня зовут " + name + ", мне " + age + " лет.");
+
+            // Ожидание нажатия клавиши, нужно чтобы окно консоли не закрывалось
+            Console.ReadKey();
+        }
+    }
+}
