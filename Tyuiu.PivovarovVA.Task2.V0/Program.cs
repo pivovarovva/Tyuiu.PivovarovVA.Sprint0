1ca579032cbcc58f8a4490d854bf11f3cb47a06f
Admin <Admin@DESKTOP-34SQLP8>
2024-09-20 18:22:17 +0500

    Task2

--- Tyuiu.PivovarovVA.Task2.V0/Program.cs
+++ Tyuiu.PivovarovVA.Task2.V0/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Microsoft.Playwright;
+
+namespace Tyuiu.PivovarovVA.Sprint0.Task2.V0.Test
+{
+    [Parallelizable(ParallelScope.Self)]
+    [TestFixture]
+    public class Tests : PageTest
+    {
+        [Test]
+        public async Task HomepageHasPlaywrightInTitleAndGetStartedLinkLinkingtoTheIntroPage()
+        {
+            await Page.GotoAsync("https://playwright.dev");
+
+            // Ожидание, что заголовок содержит "Playwright"
+            await Expect(Page).ToHaveTitleAsync(new Regex("Playwright"));
+
+            // Создание локатора для ссылки "Get Started"
+            var getStarted = Page.Locator("text=Get Started");
+
+            // Ожидание, что атрибут "href" равен "/docs/intro"
+            await Expect(getStarted).ToHaveAttributeAsync("href", "/docs/intro");
+
+            // Клик по ссылке "Get Started"
+            await getStarted.ClickAsync();
+
+            // Ожидание, что URL содержит "intro"
+            await Expect(Page).ToHaveURLAsync(new Regex(".*intro"));
+        }
+    }
+}
